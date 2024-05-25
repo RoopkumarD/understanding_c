39af5c79b3acdb211e9f7bedb1501b7e1dd493fa
RoopkumaD <dasroopkumar5@gmail.com>
2024-05-26 00:33:36 +0530

    little change due to change in config of my machine

--- csv_reader/compare.py
+++ csv_reader/compare.py
@@ -1,23 +1,28 @@
-import pandas as pd
-import subprocess
 import os
+import subprocess
 import sys
-import math
-import numpy as np
 
-pd.set_option('display.precision', 18)
+import pandas as pd
+
+pd.set_option("display.precision", 18)
 
 directory_name = "csvs/"
 
+
 def main(csv_file: list[str]):
     precision_threshold = 1e-10
-    csv_files = [
-        directory_name + filename 
-        for filename in os.listdir(directory_name) if ".csv" in filename
-    ] if csv_file == None else csv_file
+    csv_files = (
+        [
+            directory_name + filename
+            for filename in os.listdir(directory_name)
+            if ".csv" in filename
+        ]
+        if csv_file == None
+        else csv_file
+    )
 
     for filename in csv_files:
-        p = subprocess.run(['./small_buffer_read_csv.out', filename, '1'])
+        p = subprocess.run(["./csv_reader.out", filename, "1"])
         if p.returncode != 0:
             exit(1)
         print(f"CSV FILE: {filename}")
@@ -29,22 +34,24 @@ def main(csv_file: list[str]):
 def compare_two_result(orig_csv, interpreted_csv, precision_threshold):
     csv1 = pd.read_csv(orig_csv)
     csv2 = pd.read_csv(interpreted_csv, header=None)
-    
+
     features = list(csv1)
-    
+
     for i in range(len(features)):
         df1 = csv1.loc[:, features[i]]
         df2 = csv2.loc[:, i]
-    
+
         combined_view = {"orig": df1, "interpreted": df2}
         combined_view = pd.DataFrame(combined_view, index=df1.index)
-    
-        idx = abs(combined_view['orig'] - combined_view['interpreted']) < precision_threshold
+
+        idx = (
+            abs(combined_view["orig"] - combined_view["interpreted"])
+            < precision_threshold
+        )
         if idx.all() == False:
             print(f"For feature: {features[i]}, feature_num: {i}")
             print(combined_view[~idx])
             print("\n-------------------------------------------\n")
-
 
 
 if __name__ == "__main__":
