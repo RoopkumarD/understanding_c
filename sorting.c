826bdc6d3265895da147aa42b65f3871090d5b63
RoopkumaD <dasroopkumar5@gmail.com>
2024-06-12 13:05:53 +0530

    added insertion sort

--- sorting.c
+++ sorting.c
@@ -3,6 +3,53 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef struct ll {
+    int num;
+    struct ll *nxt;
+} LinkedList;
+
+void insertion_sort(int *arr, size_t arr_length) {
+    LinkedList *nodes = malloc(sizeof(LinkedList[arr_length + 1]));
+    if (nodes == NULL) {
+        printf("Couldn't assign memory for nodes\n");
+        return;
+    }
+    LinkedList *pointer = nodes;
+    LinkedList *base = pointer;
+    base->num = 0;
+    base->nxt = NULL;
+    LinkedList *otherHead = pointer + 1;
+
+    for (size_t i = 0; i < arr_length; i++) {
+        LinkedList *elem = ++pointer;
+        elem->num = arr[i];
+        elem->nxt = NULL;
+
+        // insertion time
+        LinkedList *before = base;
+        LinkedList *curr = base->nxt;
+        while (curr != NULL) {
+            if (curr->num > elem->num) {
+                break;
+            }
+            before = curr;
+            curr = curr->nxt;
+        }
+        elem->nxt = before->nxt;
+        before->nxt = elem;
+    }
+
+    LinkedList *curr = base->nxt;
+    size_t i = -1;
+    while (curr != NULL) {
+        arr[++i] = curr->num;
+        curr = curr->nxt;
+    }
+
+    free(nodes);
+    return;
+}
+
 // O(N^2) time and O(1) space
 void selection_sort(int *arr, size_t arr_length) {
     for (int i = 0; i < arr_length - 1; i++) {
@@ -40,20 +87,22 @@ void shuffle(int *arr, size_t arr_length);
 int main(void) {
     srand(time(NULL));
 
-    int arr[100];
-    for (int i = 0; i < 100; i++) {
+    size_t arr_length = 100;
+    int arr[arr_length];
+    for (int i = 0; i < arr_length; i++) {
         arr[i] = i;
     }
 
     // shuffling
-    shuffle(arr, 100);
+    shuffle(arr, arr_length);
 
     // sorting
-    // bubble_sort(arr, 100);
-    selection_sort(arr, 100);
+    // bubble_sort(arr, arr_length);
+    // selection_sort(arr, arr_length);
+    insertion_sort(arr, arr_length);
 
     // checking sorting
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < arr_length; i++) {
         printf("%d, ", arr[i]);
     }
     printf("\n");
